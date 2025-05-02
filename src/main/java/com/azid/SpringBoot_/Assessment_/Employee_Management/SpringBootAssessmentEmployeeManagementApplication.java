59907c000ad88c8ff3eb7e02d53e4b0a8f85a7cc
jun.yang.tee <jun.yang.tee@accenture.com>
2025-05-02 11:46:19 +0800

    first commit

---
+++ src/main/java/com/azid/SpringBoot_/Assessment_/Employee_Management/SpringBootAssessmentEmployeeManagementApplication.java
@@ -0,0 +1,13 @@
+package com.azid.SpringBoot_.Assessment_.Employee_Management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAssessmentEmployeeManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAssessmentEmployeeManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/azid/SpringBoot_/Assessment_/Employee_Management/SpringBootAssessmentEmployeeManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.azid.SpringBoot_.Assessment_.Employee_Management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAssessmentEmployeeManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
